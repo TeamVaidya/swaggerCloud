b05ee2e4354ea6e9d0dab5cf0daf56cb52e51937
Samyakjain2004 <samyakjainsamy2004@gmail.com>
2025-02-28 20:43:33 +0530

    deploy

---
+++ SwaggerCloud/src/test/java/com/vaidya/SwaggerCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaidya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
